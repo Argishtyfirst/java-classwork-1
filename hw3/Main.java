8633e4e8511dc865b5a0f5506116224ccf19b61e
argishtyfirst <argishtyfirst@gmail.com>
2018-07-10 11:59:52 +0400

    HW2 and HW3 in one folder

---
+++ hw3/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main{
+	static int factorial(int n){
+		if(n==1){
+			return 1;
+		}else{
+			return n * factorial(n-1);
+		}
+	}
+	public static void main(String[] args){
+		Scanner number = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n = number.nextInt();
+		int fact = factorial(n);
+		System.out.println(fact);
+	}
+}
